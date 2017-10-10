c53907f80fc0db7975553c143b9e10de69bfde88
Aidar Ibatullin <amazing.space.invader@gmail.com>
2017-10-10 18:43:01 +0500

    Closes #3

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from './containers/app/App';
 
@@ -8,11 +8,18 @@ import SignUp from './containers/auth/SignUp';
 import SignIn from './containers/auth/SignIn';
 import RestorePassword from './containers/auth/RestorePassword';
 
+import AppWrapper from './containers/app/AppWrapper';
+
 export const namedRoutes = {
   base: '/',
   signIn: '/auth/signin',
   signUp: '/auth/signup',
-  password: '/auth/password'
+  password: '/auth/password',
+  dashboard: '/dashboard',
+  transactions: '/dashboard/transactions',
+  referrals: '/dashboard/referrals',
+  sendTokens: '/dashboard/send-tokens',
+  account: '/dashboard/account'
 };
 
 const routes = (
@@ -23,6 +30,14 @@ const routes = (
       </Route>
       <Route path="signin" component={SignIn}/>
       <Route path="password" component={RestorePassword}/>
+    </Route>
+
+    <Route path="dashboard" component={AppWrapper}>
+      <IndexRoute component={null}/>
+      <Route path="transactions" component={null}/>
+      <Route path="referrals" component={null}/>
+      <Route path="send-tokens" component={null}/>
+      <Route path="account" component={null}/>
     </Route>
   </Route>
 );
