b65bc6d50743506e754fb51b05045e08d34f662b
Aidar Ibatullin <amazing.space.invader@gmail.com>
2017-11-01 00:22:28 +0500

    Dockerfile Production

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,5 @@ FROM mhart/alpine-node:8
 
 ARG ENV_FILE=stage
 
+VOLUME /usr/src/app
 WORKDIR /usr/src/app
-ADD . /usr/src/app/
-RUN cp -r .env.${ENV_FILE} .env
-
-RUN yarn
-RUN yarn run build
--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 set -ex
-IMAGE_NAME="jincort/frontend-supreme-happiness"
+DEV_IMAGE_NAME="jincort/frontend-supreme-happiness-develop"
+PROD_IMAGE_NAME="jincort/frontend-supreme-happiness"
 TAG="${1}"
-ENV_FILE="${2}"
 
-docker build --build-arg ENV_FILE=${ENV_FILE} -t ${IMAGE_NAME}:${TAG} .
-docker push ${IMAGE_NAME}:${TAG}
+docker push ${DEV_IMAGE_NAME}:${TAG}
+docker build -f Dockerfile.prod --no-cache -t ${PROD_IMAGE_NAME}:${TAG} .
+docker push ${PROD_IMAGE_NAME}:${TAG}
