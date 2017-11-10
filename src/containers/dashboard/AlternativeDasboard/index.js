31e88241ad60e8719afbd6dfbaaa5337beb066ba
Aidar Ibatullin <amazing.space.invader@gmail.com>
2017-11-10 17:34:16 +0500

    Update alternative dashboard

--- src/containers/dashboard/AlternativeDasboard/index.js
+++ src/containers/dashboard/AlternativeDasboard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import s from './styles.css';
 
@@ -6,98 +6,135 @@ import { openMakeDepositPopup } from '../../../redux/modules/app/makeDepositPopu
 
 import Button from '../../../components/common/Button';
 
-const AlternativeDashboard = (props) => {
-  const { openMakeDepositPopup } = props;
+class AlternativeDashboard extends Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className={s.dash}>
-      <div className={s.title}>
-        Jincor ICO starts on November 15
-      </div>
+    this.state = {
+      more: false
+    };
+  }
 
-      <div className={s.subtitle}>
-        You can buy tokens when ICO starts. We will notify you on your email.<br/>
-        Now you can make deposit to your wallet.
-      </div>
+  render() {
+    const { openMakeDepositPopup } = this.props;
+    const { more } = this.state;
 
-      <div className={s.button}>
-        <Button size="small" onClick={() => openMakeDepositPopup()}>Make deposit</Button>
-      </div>
+    const renderSubtitle = () =>
+      (more
+        ? (
+          <div className={s.subtitle}>
+          The purchase of the tokens will be available after the beginning of ICO.<br/>
+          You’ll receive an email notification at the date of the crowdsale. All
+          participants of the crowdsale are obliged to go through KYC/AML verification.<br/><br/>
+          The details on how to go through this procedure and step-by-step guidelines on how
+          to participate in the crowdsale will be provided closer to the date of ICO.<br/><br/>
+          To secure your share of JCR tokens make a deposit to your Jincor wallet now.
+          </div>
+        )
+        : (
+          <div className={s.subtitle}>
+            The purchase of the tokens will be available after the beginning of ICO.<br/>
+            You’ll receive an email notification at the date of
+            the… <a onClick={() => this.setState({ more: true })}>more</a>
+          </div>
+        ));
 
-      <div className={s.section}>
-        <div className={s.sectionTitle}>JCR Token Price</div>
+    return (
+      <div className={s.dash}>
+        <div className={s.title}>
+          Jincor ICO starts on December 1
+        </div>
 
-        <div className={s.blocks}>
-          <div className={s.block}>
-            <div className={s.now}>Now</div>
-            <div className={s.wrap}>
-              <div className={s.val}>$1</div>
-              <div className={s.lbl}>ICO price</div>
+        {renderSubtitle()}
+
+        <div className={s.button}>
+          <Button size="small" onClick={() => openMakeDepositPopup()}>Make deposit</Button>
+        </div>
+
+        <div className={s.section}>
+          <div className={s.sectionHead}>
+            <div className={s.sectionTitle}>JCR Token Price Projection</div>
+            <div className={s.sectionSubtitle}>
+              This moderate revenue projection for JCR token value based on businesses demand
+              for cryptocurrency payment solutions trends and speed of spreading the smart
+              contracts. For more details please review the Whitepaper.
             </div>
           </div>
 
-          <div className={s.block}>
-            <div className={s.up}/>
-            <div className={s.wrap}>
-              <div className={s.val}>$5</div>
-              <div className={s.lbl}>In 2 years</div>
+          <div className={s.blocks}>
+            <div className={s.block}>
+              <div className={s.now}>Now</div>
+              <div className={s.wrap}>
+                <div className={s.val}>$1</div>
+                <div className={s.lbl}>ICO price</div>
+              </div>
             </div>
-          </div>
 
-          <div className={s.block}>
-            <div className={s.up}/>
-            <div className={s.wrap}>
-              <div className={s.val}>$13</div>
-              <div className={s.lbl}>In 4 years</div>
+            <div className={s.block}>
+              <div className={s.up}/>
+              <div className={s.wrap}>
+                <div className={s.val}>$5</div>
+                <div className={s.lbl}>In 2 years</div>
+              </div>
+            </div>
+
+            <div className={s.block}>
+              <div className={s.up}/>
+              <div className={s.wrap}>
+                <div className={s.val}>$13</div>
+                <div className={s.lbl}>In 4 years</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className={s.section}>
-        <div className={s.sectionTitle}>Corporate clients</div>
-
-        <div className={s.blocks}>
-          <div className={s.block}>
-            <div className={s.now}>Now</div>
-            <div className={s.wrap}>
-              <div className={s.val}>300+</div>
-              <div className={s.lbl}>Beta access requests</div>
-            </div>
+        <div className={s.section}>
+          <div className={s.sectionHead}>
+            <div className={s.sectionTitle}>Corporate clients</div>
           </div>
 
-          <div className={s.block}>
-            <div className={s.up}/>
-            <div className={s.wrap}>
-              <div className={s.val}>1K</div>
-              <div className={s.lbl}>Companies after ICO</div>
+          <div className={s.blocks}>
+            <div className={s.block}>
+              <div className={s.now}>Now</div>
+              <div className={s.wrap}>
+                <div className={s.val}>300+</div>
+                <div className={s.lbl}>Beta access requests</div>
+              </div>
             </div>
-          </div>
 
-          <div className={s.block}>
-            <div className={s.up}/>
-            <div className={s.wrap}>
-              <div className={s.val}>20K</div>
-              <div className={s.lbl}>Companies is 2 years</div>
+            <div className={s.block}>
+              <div className={s.up}/>
+              <div className={s.wrap}>
+                <div className={s.val}>1K</div>
+                <div className={s.lbl}>Companies after ICO</div>
+              </div>
             </div>
-          </div>
 
-          <div className={s.block}>
-            <div className={s.up}/>
-            <div className={s.wrap}>
-              <div className={s.val}>50K</div>
-              <div className={s.lbl}>Companies is 4 years</div>
+            <div className={s.block}>
+              <div className={s.up}/>
+              <div className={s.wrap}>
+                <div className={s.val}>20K</div>
+                <div className={s.lbl}>Companies is 2 years</div>
+              </div>
+            </div>
+
+            <div className={s.block}>
+              <div className={s.up}/>
+              <div className={s.wrap}>
+                <div className={s.val}>50K</div>
+                <div className={s.lbl}>Companies is 4 years</div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className={s.wpLink}>
-        <a href="https://jincor.com/whitepaper" target="_blank">More info in the Whitepaper</a>
+        <div className={s.wpLink}>
+          <a href="https://jincor.com/whitepaper" target="_blank">Read the Whitepaper</a>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default connect(
   null,
