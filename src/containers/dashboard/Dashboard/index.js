0fd8dcd95f773978c1d6214e1a708578d37ca51f
Aidar Ibatullin <amazing.space.invader@gmail.com>
2017-11-15 14:00:01 +0400

    Closes #52

--- src/containers/dashboard/Dashboard/index.js
+++ src/containers/dashboard/Dashboard/index.js
@@ -4,29 +4,61 @@ import s from './styles.css';
 
 import { fetchDashboard } from '../../../redux/modules/dashboard/dashboard';
 
-// import BuyTokensForm from '../BuyTokensForm';
-// import BalanceInfo from '../BalanceInfo';
+import BuyTokensForm from '../BuyTokensForm';
+import BalanceInfo from '../BalanceInfo';
 import VerifyBuyTokensPopup from '../VerifyBuyTokensPopup';
 import AlternativeDashboard from '../AlternativeDasboard';
 import AlternativeBalanceInfo from '../AlternativeBalanceInfo';
 
+const ICO_START = new Date('Dec 1, 2017 00:00:00 GMT+0300').getTime();
+const ENABLE_TIMER = new Date(ICO_START - (60 * 60 * 1000));
+const DISABLE_TIMER = new Date(ICO_START + (10 * 60 * 1000));
+
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentTime: 0,
+      icoStarted: false
+    };
+
+    this._tick = this._tick.bind(this);
+  }
+
   componentWillMount() {
     const { fetchDashboard } = this.props;
 
     fetchDashboard();
+
+    const currentTime = new Date();
+
+    if (currentTime > ENABLE_TIMER && currentTime < DISABLE_TIMER) {
+      setInterval(this._tick, 1000);
+    }
+  }
+
+  _tick() {
+    const currentTime = new Date();
+    this.setState({ currentTime });
+
+    if (currentTime > ICO_START) {
+      this.setState({ icoStarted: true });
+    }
   }
 
   render() {
+    const { icoStarted } = this.state;
+
     return (
       <div className={s.wrapper}>
         <div className={s.main}>
           <div className={s.buyTokensForm}>
-            <AlternativeDashboard/>
+            {icoStarted ? <BuyTokensForm/> : <AlternativeDashboard/>}
           </div>
         </div>
         <div className={s.col}>
-          <AlternativeBalanceInfo/>
+          {icoStarted ? <BalanceInfo/> : <AlternativeBalanceInfo/>}
         </div>
 
         <VerifyBuyTokensPopup/>
