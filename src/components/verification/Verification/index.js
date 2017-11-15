1604edb7811e5da1194ca490cf21789f5f63ac1c
Aidar Ibatullin <amazing.space.invader@gmail.com>
2017-11-15 22:05:41 +0400

    Update kyc

--- src/components/verification/Verification/index.js
+++ src/components/verification/Verification/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import loadScript from '../../../utils/scriptLoader';
 import s from './styles.css';
 
@@ -26,25 +27,75 @@ class Verification extends Component {
               authorizationToken
             }).initVerify('jumio');
           })
-          .catch(({ error }) => {
-            this.setState({ error });
+          .catch((e) => {
+            this.setState({ error: e.error });
           });
       });
   }
 
   render() {
-    const { error } = this.state;
+    const { kycStatus } = this.props;
+
+    console.log(kycStatus);
+
+    const renderPage = () => {
+      switch (kycStatus) {
+        case 'pending':
+          return renderPending();
+        case 'max_attempts_reached':
+          return renderReachLimit();
+        case 'verified':
+          return renderSuccess();
+        default:
+          return renderPlugin();
+      }
+    };
+
+    const renderPending = () => (
+      <div className={s.status}>
+        <div className={s.title}>Your account is being verified…</div>
+        <div className={s.text}>
+          Your documents are successfully uploaded and being processed now.
+          This may take up to 15 minutes, please be patient and don’t try to
+          relaunch the verification process.
+        </div>
+      </div>
+    );
+
+    const renderReachLimit = () => (
+      <div className={s.status}>
+        <div className={s.title}>Maximum attempts reached</div>
+        <div className={s.text}>
+          There were some issues with verifying your account.
+          Please, contact our team directly
+          at <a href="mailto:support@jincor.com">support@jincor.com</a> to
+          proceed with the process.
+        </div>
+      </div>
+    );
+
+    const renderSuccess = () => (
+      <div className={s.status}>
+        <div className={s.title}>Account verification complete</div>
+        <div className={s.text}>
+          Your personal data has been verified successfully,
+          and now you have full access to Jincor crowdsale.
+        </div>
+      </div>
+    );
+
+    const renderPlugin = () => (
+      <div id="jumio"/>
+    );
 
     return (
       <div>
-        {error ? <div className={s.error}>
-          {error}<br/>
-          <a href="mailto:support@jincor.com">support@jincor.com</a>
-        </div> : null}
-        <div id="jumio"/>
+        {renderPage()}
       </div>
     );
   }
 }
 
-export default Verification;
+export default connect((state) => ({
+  kycStatus: state.app.app.user.kycStatus
+}))(Verification);
