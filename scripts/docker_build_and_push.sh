7549e906a842e82aa4942da51638a1f8c16a8ba8
Alexander <aleksand-isaev@yandex.ru>
2018-04-10 16:30:34 +0400

    Fixed travis yml

--- scripts/docker_build_and_push.sh
+++ scripts/docker_build_and_push.sh
@@ -7,7 +7,7 @@ echo $TRAVIS_PULL_REQUEST
 # if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
   export DOCKERFILE=`if [ "$TRAVIS_BRANCH" == "develop" ]; then echo "Dockerfile.stage";
     else echo "Dockerfile.prod" ; fi`
-  docker build -f $DOCKERFILE --no-cache -t jincort/frontend-ico-dashboard:${TAG} .
+  docker build -f $DOCKERFILE1 --no-cache -t jincort/frontend-ico-dashboard:${TAG} .
   docker login -u $DOCKER_USER -p $DOCKER_PASS
   docker push jincort/frontend-ico-dashboard-develop:${TAG}
   docker push jincort/frontend-ico-dashboard:${TAG}
