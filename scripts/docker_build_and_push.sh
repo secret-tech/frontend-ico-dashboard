88e20d39ac21b3af507c6f9a3ab54b8eca3cbf8f
Alexander <aleksand-isaev@yandex.ru>
2018-04-10 12:52:39 +0400

    fixed travis script

--- scripts/docker_build_and_push.sh
+++ scripts/docker_build_and_push.sh
@@ -2,13 +2,15 @@
 
 set -ev
 
-if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
+
+echo $TRAVIS_PULL_REQUEST
+# if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
   export DOCKERFILE=`if [ "$TRAVIS_BRANCH" == "develop" ]; then echo "Dockerfile.stage";
     else echo "Dockerfile.prod" ; fi`
   docker build -f $DOCKERFILE --no-cache -t jincort/frontend-ico-dashboard:${TAG} .
   docker login -u $DOCKER_USER -p $DOCKER_PASS
   docker push jincort/frontend-ico-dashboard-develop:${TAG}
   docker push jincort/frontend-ico-dashboard:${TAG}
-fi
+# fi
 
 exit 0;
