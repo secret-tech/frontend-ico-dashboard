c22ca537cf2900426c437a126db74f4a14f67f75
Alexander <aleksand-isaev@yandex.ru>
2018-04-17 13:18:16 +0400

    2FA popup was fixed

--- src/containers/settings/EnableTwoFactorAuthPopup/index.js
+++ src/containers/settings/EnableTwoFactorAuthPopup/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
+import { Button, Intent } from '@blueprintjs/core';
+import cx from 'classnames';
 import s from './styles.css';
 
 import { twoFactorCode } from '../../../utils/validators';
@@ -13,7 +15,6 @@ import {
 
 import Popup from '../../../components/common/Popup';
 import RenderInput from '../../../components/forms/RenderInput';
-import Button from '../../../components/common/Button';
 
 class EnableTwoFactorAuthPopup extends Component {
   componentWillReceiveProps(nextProps) {
@@ -45,47 +46,43 @@ class EnableTwoFactorAuthPopup extends Component {
       <Popup
         title={t('enableTwoFactorAuth')}
         open={open}
-        close={() => closeEnableTwoFactorAuthPopup()}>
+        close={() => closeEnableTwoFactorAuthPopup()}
+        style={{ width: '300px' }}>
 
-        <div className={s.body}>
-          <div className={s.description}>
-            {t('twoAuthDescription')}
-          </div>
-
-          <div className={s.qr}>
-            <img src={qrPngDataUri}/>
-          </div>
-
-          <form onSubmit={handleSubmit(verifyEnableTwoFactorAuth)}>
-            <div className={s.field}>
-              <Field
-                component={RenderInput}
-                name="code"
-                placeholder={t('code')}
-                validate={twoFactorCode}/>
-            </div>
-
-            <Field
-              component={RenderInput}
-              name="verificationId"
-              type="hidden"
-              disabled/>
-
-            <Field
-              component={RenderInput}
-              name="method"
-              type="hidden"
-              disabled/>
-
-            <div className={s.button}>
-              <Button
-                type="submit"
-                spinner={spinner}
-                disabled={invalid}>{t('enable')}</Button>
-            </div>
-          </form>
+        <div className={cx(s.description, 'pt-text-muted')}>
+          {t('twoAuthDescription')}
         </div>
 
+        <div className={s.qr}>
+          <img src={qrPngDataUri} />
+        </div>
+
+        <form onSubmit={handleSubmit(verifyEnableTwoFactorAuth)}>
+          <Field
+            component={RenderInput}
+            name="code"
+            placeholder={t('code')}
+            validate={twoFactorCode} />
+
+          <Field
+            component={RenderInput}
+            name="verificationId"
+            type="hidden"
+            disabled />
+
+          <Field
+            component={RenderInput}
+            name="method"
+            type="hidden"
+            disabled />
+
+          <Button
+            className="pt-fill"
+            type="submit"
+            intent={Intent.PRIMARY}
+            loading={spinner}
+            disabled={invalid}>{t('enable')}</Button>
+        </form>
       </Popup>
     );
   }
