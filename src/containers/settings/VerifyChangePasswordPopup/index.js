2487a7b680414b6572fe61263d0e4de35ec5d284
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-05-21 19:47:31 +0500

    Fix verification request [2]

--- src/containers/settings/VerifyChangePasswordPopup/index.js
+++ src/containers/settings/VerifyChangePasswordPopup/index.js
@@ -37,6 +37,7 @@ const VerifyChangePasswordPopup = (props) => {
           oldPassword,
           newPassword,
           verification: {
+            method,
             verificationId
           }
         }}/>
