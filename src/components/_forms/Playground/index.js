432684bd195427786e171699097bf255550d3b5f
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-05-23 07:09:05 +0500

    Add new password renderer

--- src/components/_forms/Playground/index.js
+++ src/components/_forms/Playground/index.js
@@ -3,7 +3,7 @@ import { reduxForm, Field } from 'redux-form';
 import { Button, Intent } from '@blueprintjs/core';
 
 import RenderInput from '../RenderInput';
-import RenderPassword from '../../_forms/RenderPassword';
+import RenderPassword from '../RenderPassword';
 
 import { required, twoFactorCode } from '../../../utils/validators';
 import s from './styles.scss';
@@ -40,6 +40,16 @@ const Playground = (props) => {
           label="2FA input"
           validate={twoFactorCode}
           tip="You may input only 6 digits"/>
+
+        <Field
+          name="password"
+          component={RenderPassword}
+          large
+          leftIcon="lock"
+          placeholder="Your password"
+          label="Password"
+          validate={required}
+          tip="Password must be at least 8 characters length, contain at least one number, one capital letter, one small letter. Special characters are allowed."/>
 
         {/* <Field
           name="second"
--- src/components/_forms/RenderPassword/index.js
+++ src/components/_forms/RenderPassword/index.js
@@ -1,100 +1,20 @@
 import React, { Component } from 'react';
-import classnames from 'classnames/bind';
-
 import { Button } from '@blueprintjs/core';
-
-import s from './styles.scss';
+import RenderInput from '../RenderInput';
 
 class RenderPassword extends Component {
   constructor(props) {
     super(props);
-
     this.state = { type: 'password' };
   }
 
   render() {
-    const {
-      label,
-      input,
-      meta,
-      icon,
-      size,
-      tip,
-      ...restProps
-    } = this.props;
-
-    const {
-      error,
-      invalid,
-      active,
-      dirty
-    } = meta;
-
-    const isInvalid = () => {
-      if (!active && invalid && dirty) return true;
-      if (!invalid) return false;
-
-      return null;
-    };
-
-    const inputClassName = classnames(
-      'pt-input',
-      'pt-fill',
-      isInvalid() ? 'pt-intent-danger' : null
-    );
-
-    const formGroupClassName = classnames(
-      'pt-form-group',
-      isInvalid() ? 'pt-intent-danger' : null
-    );
-
-    const buttonClassName = classnames(
-      'pt-button',
-      size === 'pt-large' ? 'pt-large' : null
-    );
-
-    const renderButton = () => {
-      if (this.state.type === 'password') {
-        return (
-          <Button
-            className={buttonClassName}
-            icon="eye-open"
-            onClick={() => this.setState({ type: 'text' })}/>
-        );
-      }
-
-      return (
-        <Button
-          className={buttonClassName}
-          icon="eye-off"
-          onClick={() => this.setState({ type: 'password' })}/>
-      );
-    };
+    const renderButton = () => (this.state.type === 'password'
+      ? <Button minimal icon="eye-open" onClick={() => this.setState({ type: 'text' })}/>
+      : <Button minimal icon="eye-off" onClick={() => this.setState({ type: 'password' })}/>);
 
     return (
-      <div className={formGroupClassName}>
-        {label
-          ? (<label className="pt-label">
-              {label}
-            </label>)
-          : null}
-
-        <div className="pt-control-group">
-          <div className="pt-input-group pt-fill">
-            <div className="pt-form-content">
-              <input className={inputClassName} {...input} {...restProps} type={this.state.type}/>
-              {isInvalid() ? <div className="pt-form-helper-text">{error}</div> : null}
-            </div>
-          </div>
-          {renderButton()}
-        </div>
-
-        {!tip ? null : (<div className={s.tip}>
-          Password must be at least 8 characters length, contain at
-          least one number, one capital letter, one small letter.
-          Special characters are allowed.
-        </div>)}
-      </div>
+      <RenderInput {...this.props} rightElement={renderButton()} type={this.state.type}/>
     );
   }
 }
